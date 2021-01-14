8930f923f04723fb3f0ade31ae752409f8895be9
octos-network <octochicken7aber3@gmail.com>
2021-01-14 14:43:03 +0100

    onDraw gepixelt

--- app/src/main/java/com/android/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/android/example/minipaint/MyCanvasView.kt
@@ -1,7 +1,24 @@
 package com.android.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.support.v4.content.res.ResourcesCompat
 import android.view.View
 
 class MyCanvasView(context: Context) : View(context) {
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
 }
