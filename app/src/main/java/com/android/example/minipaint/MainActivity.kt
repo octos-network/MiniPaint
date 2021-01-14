def26a6dcbd5dd509de03b5dbcf1705cfbdccde1
octos-network <octochicken7aber3@gmail.com>
2021-01-14 14:17:33 +0100

    Von der Schabone abgezeichnet

---
+++ app/src/main/java/com/android/example/minipaint/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.example.minipaint
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+    }
+}
