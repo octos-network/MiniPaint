87982f0972ffc761e3e8da702990c4e0c242ef14
octos-network <octochicken7aber3@gmail.com>
2021-01-15 13:50:46 +0100

    The Bad Touch

--- app/src/main/java/com/android/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/android/example/minipaint/MyCanvasView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.support.v4.content.res.ResourcesCompat
+import android.view.MotionEvent
 import android.view.View
 
 private const val STROKE_WIDTH = 12f // has to be float
@@ -34,6 +35,12 @@ class MyCanvasView(context: Context) : View(context) {
 
     private var path = Path()
 
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
@@ -45,5 +52,28 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {}
+
+    private fun touchUp() {}
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
     }
 }
